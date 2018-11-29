ad2e03ce7159dd321e140c68b8e964da06820dc8
Joseph Wisor <joseph.wisor@gmail.com>
2018-11-28 18:15:00 -0600

    Algorithm side finished out

--- frontend/src/app/resource-list-page/algorithm-list/algorithm-list.component.ts
+++ frontend/src/app/resource-list-page/algorithm-list/algorithm-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {MatSnackBar} from '@angular/material';
 
 export interface Algorithm {
   algoName: string;
@@ -26,44 +27,98 @@ const ALGO_DATA: Algorithm[] = [
 })
 export class AlgorithmListComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, public snackBar: MatSnackBar) { }
+  
+  ALGORITHM_DATA: Algorithm[] = [
+    { algoName: 'Algorithm 1', description: 'I do stuff!' },
+    { algoName: 'Algorithm 2', description: 'I do more stuff!' },
+    { algoName: 'Algorihtm 3', description: 'I accomplish many things!' },
+    { algoName: 'Algorithm 4', description: 'I do some stuff.' },
+  ];
 
+  currentLeftSidenav: string;
+  currentRightSidenav: string = "createNew";
   uploadSidenavOpened: boolean;
   detailSidenavOpened: boolean;
 
-  displayedColumns: string[] = ['algoName', 'description'];
-  dataSource = ALGO_DATA;
+  displayedColumns: string[] = ['name', 'description'];
+  //dataSource = new MatTableDataSource<Algorithm>();
+  dataSource = this.ALGORITHM_DATA;
   resourceType = 'Algorithm';
 
-  // Detail Algo Sidenav
-  selectedAlgoName: string;
-  selectedAlgoDesc: string;
+
+  // Detail Algorithm Sidenav
+  selectedAlgorithmName: string;
+  selectedAlgorithmDesc: string;
   versions: VersionItem[] = [
-    {versionName: 'V1'},
-    {versionName: 'V2'},
-    {versionName: 'V3'},
-    {versionName: 'V4'},
+    { versionName: 'V1' },
+    { versionName: 'V2' },
+    { versionName: 'V3' },
+    { versionName: 'V4' },
   ];
-  permissions: PermissionItem[] = [
-    {name: 'Joe'},
-    {name: 'Josh'},
-    {name: 'Adriana'},
-    {name: 'Justin'},
-  ]
-  openDetailSidenav(algoToOpen, algoDesc) {
+  openDetailSidenav(algorithmToOpen, algorithmDesc) {
     // Get the single algorithm
     // need name, description, list of versions, and list of permissions
+    this.currentLeftSidenav = 'detail';
     this.uploadSidenavOpened = false;
     this.detailSidenavOpened = true;
-    this.selectedAlgoName = algoToOpen;
-    this.selectedAlgoDesc = algoDesc;
+    this.selectedAlgorithmName = algorithmToOpen;
+    this.selectedAlgorithmDesc = algorithmDesc;
   }
+
+  addVersion() {
+    this.uploadSidenavOpened = true;
+    this.currentRightSidenav = 'createVersion';
+  }
+
+  createNewVersion(name: string, verName: string) {
+    this.uploadSidenavOpened = false;
+    this.selectedDataVersionName = "";
+    this.openSnackBar(name, verName);
+  }
+
+  openSnackBar(name: string, verName: string) {
+    this.snackBar.open(verName + " - Created", "CLOSE", {
+      duration: 4000,
+    });
+  }
+
+  openDownloadSnackBar(name: string, verName: string) {
+    this.snackBar.open(this.selectedAlgorithmName + " - Downloading", "CLOSE", {
+      duration: 4000,
+    });
+  }
+
+  // Version Algorithm Sidenav
+  selectedDataVersionName: string;
+  openVersion(versionName) {
+    this.uploadSidenavOpened = false;
+    this.currentLeftSidenav = 'version';
+    this.selectedDataVersionName = versionName;
+  }
+
 
   // Upload Algo Sidenav
   openUploadSidenav() {
     this.detailSidenavOpened = false;
     this.uploadSidenavOpened = true;
   }
+
+  createNewAlgorithm(name, description) {
+    let newAlgorithm: Algorithm = { algoName: name, description: description };
+    this.ALGORITHM_DATA.push(newAlgorithm);
+    this.dataSource = this.ALGORITHM_DATA;
+    this.uploadSidenavOpened = false;
+    this.selectedDataVersionName = "";
+    this.openSnackBar(name, name);
+  } 
+
+  permissions: PermissionItem[] = [
+    {name: 'Joe'},
+    {name: 'Josh'},
+    {name: 'Adriana'},
+    {name: 'Justin'},
+  ]
 
   user: string;
   getAllAlgorithmsUrl: string = '';
